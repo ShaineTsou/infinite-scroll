b0cf555b0c8bc6e3c58f733a8c24374ed134828a
Shaine Tsou <shaineagro06@gmail.com>
2021-09-17 22:32:36 +0800

    changed icons

---
+++ script.js
@@ -0,0 +1,6 @@
+
+const photoCount = 10;
+const photoTopics = 'travel'
+const apiKey = ''
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${photoCount}&topics=${photoTopics}`;
+
